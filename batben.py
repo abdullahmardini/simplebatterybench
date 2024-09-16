be16da60b07f31f7bd0ab977a78a681f98ad2357
Abdullah Mardini <superppl@gmail.com>
2024-09-15 17:03:59 -0700

    add a bit more to make this slightly more coherent, more dry

--- batben.py
+++ batben.py
@@ -4,8 +4,28 @@ stfu pylint
 '''
 
 import subprocess
+import time
+import functools
 import re
 import psutil
+
+
+def get_battery_energy():
+    '''
+    Would also be nice to know how much Wh the thing currently has
+    '''
+    energy = None
+    try:
+        output = subprocess.check_output(['upower', '-i', '/org/freedesktop/UPower/devices/battery_BAT0'])
+        lines = output.decode('utf-8').split('\n')
+        for line in lines:
+            if 'energy:' in line:
+                energy = float(line.split(':')[1].strip().split()[0])
+        return energy
+    except Exception as e:
+        print(f"Error: {e}")
+        exit(1)
+
 
 def get_battery_level():
     '''
@@ -16,6 +36,24 @@ def get_battery_level():
         print("No battery found or can't ready battery level...")
         exit(1)
     return battery.percent
+
+
+def measure_battery_life(func):
+    '''
+    Just tells us how much battery we spend during stuff
+    '''
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        initial_battery = get_battery_level()
+        initial_energy = get_battery_energy()
+        result = func(*args, **kwargs)
+        final_battery = get_battery_level()
+        final_energy = get_battery_energy()
+        print(f"Battery life spent: {initial_battery - final_battery}%")
+        print(f"Power spent: {initial_energy - final_energy}Wh")
+        return result
+    return wrapper
+
 
 def quick_bench(time_secs):
     '''
@@ -43,26 +81,33 @@ def quick_sleep(time_secs):
     subprocess.check_call(command)
 
 
+@measure_battery_life
 def sleep_check(test_time):
     '''
     checks battery drain during sleep
     '''
-    initial_battery = get_battery_level()
     quick_sleep(test_time)
-    final_battery = get_battery_level()
-    delta_battery = initial_battery - final_battery
-    print(f"slept for {test_time} seconds and spent {delta_battery}%")
 
 
+@measure_battery_life
 def wake_check(test_time):
     '''
-    checks battery drain during use
+    checks battery drain during use.
+    This is .... kind of a crappy test.
+    My idea is that the typical developer will spend most of their
+    time using an electron app or two (and they will probably crash)
+    in addition to a browser and probably docker. This should have
+    a moderate energy impact, coupled with the occassional high
+    impact energy usage, like say compiling.
+    So what I want to do is restrict the amount of time we benchmark
+    and otherwise just sit idle, which will possibly replicate just
+    staring at vscode and thinking about the problem??? who knows
     '''
-    initial_battery = get_battery_level()
-    score = quick_bench(test_time)
-    final_battery = get_battery_level()
-    delta_battery = initial_battery - final_battery
-    print(f"perf score of {score} and spent {delta_battery}%")
+    bench_time = test_time // 5
+    wait_time = (4 * test_time ) // 5
+    score = quick_bench(bench_time)
+    time.sleep(wait_time)
+    print(f"perf score of {score}")
 
 
 def main():
@@ -73,19 +118,10 @@ def main():
     arbitrary parameter like oh it's a shell script and that's bad because i
     worship object oriented bullshit
     '''
-    initial_battery_level = get_battery_level()
-
-    print(f"Initial battery level: {initial_battery_level}%")
-
-    test_time = 300
-    sleep_check(test_time)
+    test_time = 60
+    # sleep_check(test_time)
     wake_check(test_time)
 
-    final_battery_level = get_battery_level()
-    print(f"Final battery level: {final_battery_level}%")
-
-    battery_drain = initial_battery_level - final_battery_level
-    print(f"Battery drain: {battery_drain}%")
 
 if __name__ == "__main__":
     main()
