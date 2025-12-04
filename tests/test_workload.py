2f20e30b19c79a31d2296a299f38ecdcaefece86
Abdullah Mardini <superppl@gmail.com>
2025-12-03 21:53:10 -0800

    yay

--- tests/test_workload.py
+++ tests/test_workload.py
@@ -1,7 +1,12 @@
-import pytest
-
 from batben import workload
 
 
-def test_is_prime_small():
+def test_is_prime():
     assert workload.is_prime(2) is True
+    assert workload.is_prime(4) is False
+    # seems dumb but like... I've seen LLMs propose that 6 is a prime number
+    # especially when you tell it that it shouldn't shortcut via %2 and %3
+    # It'll remove the shortcut, but change the behavior ¯\_(ツ)_/¯
+    assert workload.is_prime(6) is False
+    assert workload.is_prime(41) is True
+    assert workload.is_prime(42) is False
